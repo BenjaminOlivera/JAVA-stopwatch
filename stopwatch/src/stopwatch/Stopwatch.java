c270fb5d3375241249e784e1ec9e24c47fa617ac
Brian Benjamin Olivera Colonio <unknowsoldierbj@gmail.com>
2023-03-28 23:24:25 -0500

    Sop watch jframe

--- stopwatch/src/stopwatch/Stopwatch.java
+++ stopwatch/src/stopwatch/Stopwatch.java
@@ -1,10 +1,41 @@
 package stopwatch;
 
-public class Stopwatch {
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Stopwatch extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Stopwatch frame = new Stopwatch();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
+	/**
+	 * Create the frame.
+	 */
+	public Stopwatch() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 300, 100);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
 	}
 
 }
